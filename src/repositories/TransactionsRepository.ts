9b40c2cc326b91f30d131956b98864a9b0622429
joaononnenmacker <joaononnen@hotmail.com>
2020-04-19 11:35:54 -0300

    final version

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,67 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface allTransactions {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private allTransactions: allTransactions;
+
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+    this.allTransactions = {
+      transactions: this.transactions,
+      balance: this.balance,
+    };
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): allTransactions {
+    return this.allTransactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    this.balance.income = this.transactions.reduce((total, elemento) => {
+      if (elemento.type === 'income') {
+        return (total += elemento.value);
+      }
+      return total;
+    }, 0);
+
+    this.balance.outcome = this.transactions.reduce((total, elemento) => {
+      if (elemento.type === 'outcome') {
+        return (total += elemento.value);
+      }
+      return total;
+    }, 0);
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
